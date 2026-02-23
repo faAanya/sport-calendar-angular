10385b5c7a5d4007a96ca47f19cd552dd9b7546d
palinakirpichonak-arch <palina.kirpichonak@innowise.com>
2026-02-23 18:39:49 +0300

    chore: initital commit

---
+++ sport-calendar-frontend/src/app/components/exercise-list/exercise-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-list',
+  imports: [],
+  templateUrl: './exercise-list.html',
+  styleUrl: './exercise-list.css',
+})
+export class ExerciseList {
+
+}
---
+++ sport-calendar-frontend/src/app/components/month-view/month-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-month-view',
+  imports: [],
+  templateUrl: './month-view.html',
+  styleUrl: './month-view.css',
+})
+export class MonthView {
+
+}
